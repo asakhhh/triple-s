8d5c0da50061f1996a7a5798bacb37372f8129ee
asakhant <asakhantayev@gmail.com>
2024-10-23 19:56:37 +0500

    putBucket implemented

--- main.go
+++ main.go
@@ -8,16 +8,18 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
 // TODO: GOFUMPT
 func getBuckets(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open(filepath.Join("storage", "buckets.csv"))
+	file, err := os.Open(filepath.Join("data", "buckets.csv"))
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Could not access metadata"))
+		w.Write([]byte("500 - Internal Server Error - Could not access metadata"))
 		return
 	}
+	defer file.Close()
 
 	csvReader := csv.NewReader(file)
 	var bkts []map[string]string
@@ -25,7 +27,7 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 	_, err = csvReader.Read() // header record
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Could not access metadata"))
+		w.Write([]byte("500 - Internal Server Error - Could not access metadata"))
 		return
 	}
 
@@ -41,7 +43,7 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 	}
 	if err != io.EOF {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal error while parsing metadata"))
+		w.Write([]byte("500 - Internal Server Error - wrong metadata"))
 		return
 	}
 
@@ -60,12 +62,80 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "</ListAllMyBucketsResult>")
 }
 
-func getBucket(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GET Bucket")
+func isValidBucketName(bucketName string) bool {
+	if len(bucketName) < 3 || len(bucketName) > 63 {
+		return false
+	}
+	for _, c := range bucketName {
+		if c >= 'a' && c <= 'z' {
+			continue
+		}
+		if c >= '0' && c <= '9' {
+			continue
+		}
+		if c == '-' || c == '.' {
+			continue
+		}
+		return false
+	}
+	return true
 }
 
 func putBucket(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "PUT Bucket")
+	bucketName := r.PathValue("BucketName")
+	if !isValidBucketName(bucketName) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("400 - Bad Request - invalid bucket name"))
+		return
+	}
+
+	bucketPath := filepath.Join("data", bucketName)
+	_, err := os.Stat(bucketPath)
+	if !(err != nil && os.IsNotExist(err)) {
+		w.WriteHeader(http.StatusConflict)
+		w.Write([]byte("409 - Conflict - bucket name already exists"))
+		return
+	}
+
+	err = os.Mkdir(bucketPath, 0755)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Internal Server Error - could not create bucket"))
+		return
+	}
+
+	info, err := os.Stat(bucketPath)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Internal Server Error - could not access created bucket"))
+		return
+	}
+
+	file, err := os.OpenFile(filepath.Join("data", "buckets.csv"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Internal Server Error - Could not access metadata"))
+		return
+	}
+	defer file.Close()
+	modTime := info.ModTime()
+	modTimeToString := strconv.Itoa(modTime.Year()) + "-" + strconv.Itoa(int(modTime.Month())) + "-" + strconv.Itoa(modTime.Day()) + "T" + strconv.Itoa(modTime.Hour()) + ":" + strconv.Itoa(modTime.Minute())
+	csvWriter := csv.NewWriter(file)
+	err = csvWriter.Write([]string{bucketName, modTimeToString, modTimeToString, "Active"})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("500 - Internal Server Error - Could not update metadata"))
+		return
+	}
+	csvWriter.Flush()
+	file.Close()
+
+	fmt.Fprintln(w, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+	fmt.Fprintln(w, "<CreateBucketResult>")
+	fmt.Fprintln(w, "\t<Name>"+bucketName+"</Name>")
+	fmt.Fprintln(w, "\t<CreationTime>"+modTimeToString+"</CreationTime>")
+	fmt.Fprintln(w, "\t<Status>Active</Status>")
+	fmt.Fprintln(w, "</CreateBucketResult>")
 }
 
 func deleteBucket(w http.ResponseWriter, r *http.Request) {
@@ -93,9 +163,6 @@ func main() {
 	http.HandleFunc("/", badRequest)
 
 	http.HandleFunc("GET /{$}", getBuckets)
-
-	http.HandleFunc("GET /{BucketName}", getBucket)
-	http.HandleFunc("GET /{BucketName}/{$}", getBucket)
 
 	http.HandleFunc("PUT /{BucketName}", putBucket)
 	http.HandleFunc("PUT /{BucketName}/{$}", putBucket)
