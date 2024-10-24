df545a6ceb624ffdb49ca4c00a18e3e7cff56a08
asakhhh <asakhantayev@gmail.com>
2024-10-24 11:30:42 +0500

    Implemented a func to write http errors

--- main.go
+++ main.go
@@ -12,12 +12,22 @@ import (
 	"strconv"
 )
 
+func writeHttpError(w http.ResponseWriter, code int, errorCode string, message string) {
+	w.WriteHeader(code)
+	fmt.Fprintln(w, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+	fmt.Fprintln(w, "<Error>")
+	fmt.Fprintln(w, "\t<Code>" + errorCode + "</Code>")
+	fmt.Fprintln(w, "\t<Message>")
+	fmt.Fprintln(w, "\t\t" + message)
+	fmt.Fprintln(w, "\t</Message>")
+	fmt.Fprintln(w, "</Error>")
+}
+
 // TODO: GOFUMPT
 func getBuckets(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open(filepath.Join("data", "buckets.csv"))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
 	defer file.Close()
@@ -26,8 +36,7 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 	var bkts []map[string]string
 	_, err = csvReader.Read() // header record
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
 
@@ -44,8 +53,7 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 		fields, err = csvReader.Read()
 	}
 	if err != io.EOF {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - wrong metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Invalid content in metadata")
 		return
 	}
 
@@ -64,61 +72,57 @@ func getBuckets(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "</ListAllMyBucketsResult>")
 }
 
-func isValidBucketName(bucketName string) bool {
+func isValidBucketName(bucketName string) (bool, string) {
 	charsetAndLength := regexp.MustCompile("^[-.a-z0-9]{3,63}$")
 	if !charsetAndLength.MatchString(bucketName) {
-		return false
+		return false, "Contains invalid characters and/or too short/long"
 	}
 	ipFormat := regexp.MustCompile("^[0-9]+[.][0-9]+[.][0-9]+[.][0-9]+$")
 	if ipFormat.MatchString(bucketName) {
-		return false
+		return false, "Name must not match ip-format"
 	}
 	startHyphen := regexp.MustCompile("^-.*")
 	endHyphen := regexp.MustCompile("^.*-$")
 	if startHyphen.MatchString(bucketName) || endHyphen.MatchString(bucketName) {
-		return false
+		return false, "Name must not start or end with a hyphen"
 	}
 	consecutiveHyphens := regexp.MustCompile(".*--.*")
 	if consecutiveHyphens.MatchString(bucketName) {
-		return false
+		return false, "Name must not contain consecutive hyphens"
 	}
-	return true
+	return true, ""
 }
 
 func putBucket(w http.ResponseWriter, r *http.Request) {
 	bucketName := r.PathValue("BucketName")
-	if !isValidBucketName(bucketName) {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 - Bad Request - invalid bucket name"))
+	isValid, errMsg := isValidBucketName(bucketName)
+	if !isValid {
+		writeHttpError(w, http.StatusBadRequest, "BucketNameInvalid", "Bucket name is invalid - " + errMsg)
 		return
 	}
 
 	bucketPath := filepath.Join("data", bucketName)
 	_, err := os.Stat(bucketPath)
 	if !(err != nil && os.IsNotExist(err)) {
-		w.WriteHeader(http.StatusConflict)
-		w.Write([]byte("409 - Conflict - bucket name already exists"))
+		writeHttpError(w, http.StatusConflict, "BucketNameUnavailable", "Bucket with this name already exists")
 		return
 	}
 
 	err = os.Mkdir(bucketPath, 0755)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not create bucket"))
+		writeHttpError(w, http.StatusInternalServerError, "BucketCreationError", "Error while creating bucket")
 		return
 	}
 
 	info, err := os.Stat(bucketPath)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access created bucket"))
+		writeHttpError(w, http.StatusInternalServerError, "BucketAccessError", "Error while accessing the created bucket")
 		return
 	}
 
 	file, err := os.OpenFile(filepath.Join("data", "buckets.csv"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
 	defer file.Close()
@@ -130,8 +134,7 @@ func putBucket(w http.ResponseWriter, r *http.Request) {
 	csvWriter := csv.NewWriter(file)
 	err = csvWriter.Write([]string{bucketName, modTimeToString, modTimeToString, "Active"})
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not update metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not update metadata")
 		return
 	}
 	csvWriter.Flush()
@@ -148,12 +151,11 @@ func deleteBucket(w http.ResponseWriter, r *http.Request) {
 	bucketName := r.PathValue("BucketName")
 
 	metadata, err := os.Open(filepath.Join("data", "buckets.csv"))
-	defer metadata.Close()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
+	defer metadata.Close()
 
 	csvReader := csv.NewReader(metadata)
 	fields, err := csvReader.Read()
@@ -168,27 +170,23 @@ func deleteBucket(w http.ResponseWriter, r *http.Request) {
 		fields, err = csvReader.Read()
 	}
 	if err != io.EOF {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
 	if !exists {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 - Bad Request - bucket does not exist"))
+		writeHttpError(w, http.StatusNotFound, "BucketNotFound", "Could not delete - bucket does not exist")
 		return
 	}
 
 	bkt, err := os.Open(filepath.Join("data", bucketName))
-	defer bkt.Close()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access bucket"))
+		writeHttpError(w, http.StatusInternalServerError, "BucketAccessError", "Could not access bucket")
 		return
 	}
+	defer bkt.Close()
 	_, err = bkt.Readdirnames(1)
 	if err != io.EOF {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("400 - Bad Request - bucket not empty"))
+		writeHttpError(w, http.StatusConflict, "BucketNotEmpty", "Could not delete - bucket not empty")
 		return
 	}
 
@@ -198,24 +196,23 @@ func deleteBucket(w http.ResponseWriter, r *http.Request) {
 		successfullyDeleted = false
 	}
 	metadataWrite, err := os.OpenFile(filepath.Join("data", "buckets.csv"), os.O_WRONLY|os.O_TRUNC, 0600)
-	defer metadataWrite.Close()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Internal Server Error - could not access metadata"))
+		writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not access metadata")
 		return
 	}
+	defer metadataWrite.Close()
 	csvWriter := csv.NewWriter(metadataWrite)
 	for _, bkt := range bkts {
 		if bkt[0] != bucketName || !successfullyDeleted {
 			err = csvWriter.Write(bkt)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write([]byte("500 - Internal Server Error - could not update metadata"))
+				writeHttpError(w, http.StatusInternalServerError, "MetadataError", "Could not update metadata")
 				return
 			}
 		}
 	}
 	csvWriter.Flush()
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func getObject(w http.ResponseWriter, r *http.Request) {
